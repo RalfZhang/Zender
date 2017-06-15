d4194db59ca6d9378ae19b4654f79d289e4c8a58
unknown <zhangkang103@gmail.com>
2017-06-15 23:28:57 +0800

    update min

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
+var gulp = require('gulp')
 var babel = require('gulp-babel')
 var rename = require('gulp-rename')
-
+var uglify = require('gulp-uglify')
 
 gulp.task('js', function(){
   console.log('gulp js')
   return gulp.src('lib/Zender.js')
+    .pipe(gulp.dest('dist'))
     .pipe(babel({
       presets: ['es2015']
     }))
-    // .pipe(rename(function(path){
-    //   path.basename += '.min'
-    // }))
+    .pipe(uglify())
+    .pipe(rename(function(path){
+      path.basename += '.min'
+    }))
     .pipe(gulp.dest('dist'))
 })
